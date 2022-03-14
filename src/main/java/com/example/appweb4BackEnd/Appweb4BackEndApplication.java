1adb945a36c74dc2e01fce0201d8b75035121d45
CesarCejaC <tonycejacortes@gmail.com>
2022-03-13 21:27:57 -0600

    initial commit

---
+++ src/main/java/com/example/appweb4BackEnd/Appweb4BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.appweb4BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appweb4BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appweb4BackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appweb4BackEnd/Appweb4BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appweb4BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Appweb4BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
